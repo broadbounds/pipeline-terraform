fd612a58a62d274148eabfd812a90a0d6a42560d
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-05-17 16:07:00 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region = "us-east-2"
-  version = "v2.70.0"
-}
+   region  = "us-east-2"
+   version = "v2.70.0"
+ }
 
 resource "aws_instance" "first_ec2" {
   ami           = "ami-07a3e3eda401f8caa"
