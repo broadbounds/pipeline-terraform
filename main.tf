fe4ac3a7aa8eb81057b8c6652fcb5676e261f607
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-05-18 01:16:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 provider "aws" {
    region  = "us-east-2"
+   access_key = var.access_key
+   secret_key = var.secret_key
  }
 
 resource "aws_instance" "example" {
    ami           = "ami-07a3e3eda401f8caa"
    instance_type = "t2.micro"
 }
-
-variable "access_key" {}
-
-variable "secret_key" {}
