117d108b9f273b537b90537c00b1dfa02e0c28b7
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-05-23 20:26:04 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -155,6 +155,9 @@ resource "aws_security_group" "sg_bastion_host" {
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
    }
+   tags = {
+      Name = "sg bastion host"
+   }
 }
 
 # We create an elastic IP 
